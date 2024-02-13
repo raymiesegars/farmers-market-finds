870b1cf6159c1017920d9564665398a4fe853f1e
Raymie Segars <raymievsegarsiv@gmail.com>
2024-02-13 02:45:48 -0500

    Might break my website v1

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,7 +8,7 @@ const VendorForm = () => {
     <div className="flex flex-col items-center pt-20 pb-20">
       <div className="p-8 rounded-lg shadow-md w-full max-w-md">
         <h1 className="text-xl font-bold text-center mb-4">Vendor Sign Up</h1>
-        <form>
+        <form action={vendorFormSubmission}>
           <div className="mb-4 pb-2">
             <label htmlFor="name" className="block text-sm font-medium">Name</label>
             <input type="text" id="name" name="name" className="mt-1 block w-full rounded-md border-gray-300 shadow-sm" />
