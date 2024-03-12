747d0eaf013ee1e2f5221940b8b5cba486af42c2
Raymie Segars <raymievsegarsiv@gmail.com>
2024-03-12 03:29:37 -0400

    Bug with SSR attempt 2

--- src/app/api/vendorProfiles/route.ts
+++ src/app/api/vendorProfiles/route.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '@/lib/prisma';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function GET(req: NextApiRequest, res: NextApiResponse) {
   const vendorProfiles = await prisma.vendorProfile.findMany({
     where: {
       approved: true,
