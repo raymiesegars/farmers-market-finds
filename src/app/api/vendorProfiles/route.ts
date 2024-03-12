7dcc04940d9fb9cad952b1f984327b73d588cf18
Raymie Segars <raymievsegarsiv@gmail.com>
2024-03-12 04:00:03 -0400

    SSR Bug fix attempt 3

--- src/app/api/vendorProfiles/route.ts
+++ src/app/api/vendorProfiles/route.ts
@@ -1,13 +1,11 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '@/lib/prisma';
+import prisma from "@/lib/prisma";
 
-export default async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET() {
   const vendorProfiles = await prisma.vendorProfile.findMany({
     where: {
       approved: true,
     },
   });
 
-  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
-  return res.status(200).json(vendorProfiles);
+  return Response.json(vendorProfiles);
 }
