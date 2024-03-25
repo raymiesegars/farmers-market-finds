f576e8c8bc76832f2fc4d36a6d3ce23ac4802442
Raymie Segars <raymievsegarsiv@gmail.com>
2024-03-25 16:42:22 -0400

    update fixed spelling error in vendor application submitted

--- src/app/vendorform-submitted/page.tsx
+++ src/app/vendorform-submitted/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <main className="m-auto max-w-5xl my-10 space-y-5 px-3 text-center">
       <H1>Vendor Application Submitted</H1>
-      <p>Your vendor application has been submitted and is awaiting approval by an admin.  You will receive and email upon approval with further instructions</p>
+      <p>Your vendor application has been submitted and is awaiting approval by an admin.  You will receive an email upon approval with further instructions</p>
     </main>
   );
 }
