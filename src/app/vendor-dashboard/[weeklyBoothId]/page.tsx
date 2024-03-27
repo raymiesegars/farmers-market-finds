347e7d1bf0fd37723200abed977337f2161c3c8f
Raymie Segars <raymievsegarsiv@gmail.com>
2024-03-27 13:19:19 -0400

    updated vendor booths to display and function correctly

--- src/app/vendor-dashboard/[weeklyBoothId]/page.tsx
+++ src/app/vendor-dashboard/[weeklyBoothId]/page.tsx
@@ -1,54 +1,3 @@
-<<<<<<< Updated upstream
-// import { NextPage } from "next";
-// import { fetchWeeklyBoothById } from "../../../actions/fetchWeeklyBoothById";
-// import { WeeklyBooth } from "@prisma/client";
-// import { useSearchParams } from "next/navigation";
-
-// export const dynamic = 'force-dynamic'
-
-// interface WeeklyBoothPageProps {
-//   boothData: WeeklyBooth | null;
-// }
-
-// export async function setBoothData() {
-  
-//   const searchParams = useSearchParams();
-//   console.log(searchParams);
-//   if (!weeklyBoothId) {
-//     return {
-//       notFound: true,
-//     };
-//   }
-
-//   const boothData = await fetchWeeklyBoothById(Number(weeklyBoothId));
-
-//   return {
-//     props: {
-//       boothData: boothData || null,
-//     },
-//   };
-// };
-
-// const WeeklyBoothPage: NextPage<WeeklyBoothPageProps> = ({ boothData }) => {
-  
-//   if (!boothData) {
-//     return <div>Booth not found</div>;
-//   }
-
-//   return (
-//     <p>hi</p>
-//     // <VendorBoothForm
-//     //   date={boothData.date}
-//     //   marketId={boothData.marketId}
-//     //   weeklyBoothId={boothData.weeklyBoothId}
-//     // />
-//   );
-// };
-
-
-
-// export default WeeklyBoothPage;
-=======
 import { fetchWeeklyBoothById } from "../../../actions/fetchWeeklyBoothById";
 import VendorBoothForm from "@/components/VendorBoothForm";
 
@@ -67,4 +16,4 @@ export default async function WeeklyBoothDetails({ params }: { params: { weeklyB
     />
   );
 }
->>>>>>> Stashed changes
+
