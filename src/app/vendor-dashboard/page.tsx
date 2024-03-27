3b5cf7f5a25c6fe7a00d5edc150a58fdfe4606b4
Raymie Segars <raymievsegarsiv@gmail.com>
2024-03-27 17:37:34 -0400

    fixed un-used import path for removed files

--- src/app/vendor-dashboard/page.tsx
+++ src/app/vendor-dashboard/page.tsx
@@ -1,10 +1,8 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { GlobalGoods, globalGoodsColumns } from "./global-goods-columns"
 import { Markets, marketColumns } from "./markets-columns"
 import { MarketsDataTable } from "./markets-data-table";
-import { GlobalGoodsDataTable } from "./global-goods-data-table";
 import { useRouter } from 'next/navigation';
 
 
